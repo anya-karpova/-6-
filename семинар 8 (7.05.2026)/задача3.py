bb422380fde42062037bff943eab483b7fcba84c
Anna <anyaa.karpoova.20007@gmail.com>
2026-05-14 13:57:57 +0300

    update

--- семинар 8 (7.05.2026)/задача3.py
+++ семинар 8 (7.05.2026)/задача3.py
@@ -5,9 +5,6 @@
 # Сделайте вывод о времени работы алгоритма.
 import random
 import time
-
-arr = [random.randint(1, 100) for i in range(10)]
-
 def sort(arr):
     n = len(arr)
     for i in range(n):
@@ -15,20 +12,20 @@ def sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+arr10 = [random.randint(1, 1000) for _ in range(10)]
+print("Массив из 10 элементов:", sort(arr10))
 
-print("Массив из 10 элементов:", sort(arr))
+s = [10, 100, 1000, 10000]
+for n in s:
+    arr = [random.randint(1, 1000) for i in range(n)]
 
-all_time = 0
-
-for n in [10, 100, 1000, 10000, 100000]:
-    arr = [random.randint(1, 100) for i in range(10)]
     start = time.time()
     sort(arr)
     finish = time.time()
-    time1 = finish - start
-    all_time += time1
 
-print("Время работы: " + str(all_time))
+    time1 = finish - start
+
+print(f"n = {n}, время = {time1:.6f} сек")
 
 
 
